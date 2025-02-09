d4f67b0fc81ebd374ad51b0f9744c75b78496702
EkaMoyak1 <moyekaLogin@gmail.com>
2025-02-09 23:44:39 +0900

    v2 z1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('hello')
+def s_sq(a:int):
+    p = 4 * a
+    s = a ** 2
+    d = 2 * s ** 0.5
+    return (p, s, d)
+
+a = input('Введите сторону квадрат:')
+# нужно сначала проверить: введено число или нет. А число может содержать "."
+if a.replace('.','',1).isnumeric():
+    print(s_sq(float(a)))
+
+
+
